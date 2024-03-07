dc2edd1d026fa9c2e8ceaca470f58d29b320997a
Andrey Melchikov <andrei.melchikov25@gmail.com>
2024-03-07 22:52:51 +0400

    Links to categories are searched, as well as the title and description.

--- test.py
+++ test.py
@@ -6,17 +6,25 @@ from selenium.webdriver.common.by import By
 
 
 def main():
-    web_driver_setup = WebDriverSetup()
+    web_driver_setup = WebDriverSetup(headless=False)
     driver = web_driver_setup.setup_driver()
 
-    url = f"https://www.ilovepdf.com/ru/word_to_pdf"
+    url = f"https://www.ilovepdf.com/ru"
     driver.get(url)
-    time.sleep(3)
-    uploader_button = driver.find_element(By.CSS_SELECTOR, 'a.uploader')
-    uploader_button.click()
-    time.sleep(3)
-    driver.get
+    time.sleep(1)
+    try:
+        tools_items = [element for element in driver.find_elements(
+            By.CSS_SELECTOR, 'div.tools__item')]
+        href_items = [element.find_element(By.CSS_SELECTOR, 'a').get_attribute(
+            'href') for element in tools_items]
+        title_items = [element.find_element(By.CSS_SELECTOR, 'a').get_attribute(
+            'title') for element in tools_items]
+        description_items = [element.find_element(
+            By.CSS_SELECTOR, '.tools__item__content').text for element in tools_items]
+    except Exception as e:
+        print(e)
+    pprint(description_items)
+
 
 if __name__ == "__main__":
-   main() 
-    
+    main()
